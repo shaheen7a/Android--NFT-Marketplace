c4c3a1ffd091e7ae78dcbc1571b879c328745891
Shaheen <96109270+Shaheen121@users.noreply.github.com>
2023-03-18 23:21:28 +0530

    Card Details

--- components/index.js
+++ components/index.js
@@ -1,12 +1,22 @@
 import HomeHeader from "./HomeHeader";
 import NFTCard from "./NFTCard";
-import FocusedStatusBar from "./FocusedStatusBar";
 import { RectButton, CircleButton } from "./Button";
+import { SubInfo, People, EndDate, EthPrice, NFTTitle } from "./SubInfo";
+import DetailsBid from "./DetailsBid";
+import FocusedStatusBar from "./FocusedStatusBar";
+import DetailsDesc from './DetailsDesc';
 
 export {
   HomeHeader,
   NFTCard,
-  FocusedStatusBar,
+  SubInfo,
+  People,
+  EndDate,
   RectButton,
   CircleButton,
+  EthPrice,
+  NFTTitle,
+  DetailsBid,
+  FocusedStatusBar,
+  DetailsDesc,
 };
